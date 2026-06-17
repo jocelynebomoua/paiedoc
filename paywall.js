de914467d546f05035b1ec20224be1f780ba413e
jocelynebomoua <jocelynebomoua@proton.me>
2026-06-17 20:05:32 +0200

    Delete paywall.js

--- paywall.js
+++
@@ -1,9 +0,0 @@
-{
-  "name": "paiedoc-site",
-  "version": "1.0.0",
-  "private": true,
-  "description": "Generateur de documents RH (fiches de paie, contrats) avec paiement Stripe",
-  "dependencies": {
-    "stripe": "^16.12.0"
-  }
-}
